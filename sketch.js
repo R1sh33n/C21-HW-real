601e603481450add48b584b35f8373e51e5ad4c6
R1sh33n <96629065+R1sh33n@users.noreply.github.com>
2022-03-04 17:58:21 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -46,6 +46,6 @@ function draw() {
 
 function keyPressed(){
 	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(ball,ball.position,{x:85, y:-85});
+		Matter.Body.applyForce(ball,ball.position,{x:15, y:-15});
 	}
 }
